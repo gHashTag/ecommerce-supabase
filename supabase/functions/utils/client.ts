589721691911aae4698809a672f13759d7654e79
AndreyO <andrey@railsmuffin.com>
2024-02-25 20:51:50 +0300

    code clr

--- supabase/functions/utils/client.ts
+++ supabase/functions/utils/client.ts
@@ -2,11 +2,11 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 export const client = (req: any) => {
   const ANON_KEY_SUPABASE = Deno.env.get("SUPABASE_ANON_KEY");
-  const SET_URL = Deno.env.get("SUPABASE_URL");
+  const URL_SUPABASE = Deno.env.get("SUPABASE_URL");
 
   const authHeader = req.headers.get("Authorization")!;
   const supabaseClient = createClient(
-    SET_URL ?? "",
+    URL_SUPABASE ?? "",
     ANON_KEY_SUPABASE ??
       "",
     { global: { headers: { Authorization: authHeader } } },
